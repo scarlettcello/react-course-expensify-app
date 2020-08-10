9fe7846958d2c39bd603d43ad0877b931d087425
Carol <carolhyonlee@gmail.com>
2020-08-10 16:15:28 -0400

    Setup test database environment

---
+++ src/playground/promises.js
@@ -0,0 +1,27 @@
+const promise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+    resolve({
+      name: 'Carol',
+      age: 37
+    });
+    //reject('Something went wrong!');
+  }, 3000);
+});
+
+console.log('before');
+
+promise.then((data) => {
+  console.log('1', data);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my other promise');
+    }, 3000);
+  });
+}).then((str) => {
+  console.log('does this run?', str);
+}).catch((error) => {
+  console.log('error: ', error);
+});
+
+console.log('after');
